36ca2505b965b4ac9bfe358e969e3b886a755ff5
Simon Buckner <SimonBuckner@users.noreply.github.com>
2022-11-08 11:20:14 +0000

    Add Start-Monitor script

---
+++ Start-Monitor.ps1
@@ -0,0 +1,28 @@
+
+param (
+    $BreakValue,
+    $Delay,
+    $CodeBlock
+)
+
+while ($true) {
+    Clear-Host
+    Write-Host "Running Code Block { $CodeBlock } "
+    $res = $CodeBlock.Invoke()
+    if ($res -eq $BreakValue) {
+        Write-Host "Current Value: $res"
+        Write-Host "Desired Value: $BreakValue"
+        Write-Host "Success!!"
+        break
+    }
+    Write-Host "Current Value: $res"
+    Write-Host "Desired Value: $BreakValue"
+    Write-Host "Sleeping for $Delay seconds" -NoNewline
+    For ($i=0; $i -lt $Delay; $i++) {
+        Write-Host "." -NoNewline
+        Start-Sleep 1
+    }
+}
+
+
+
